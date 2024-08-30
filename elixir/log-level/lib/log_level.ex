435e53f94b7c4b9aff2d91f879b8f51f00508d8b
Rodrigo Couto <r@rodg.co>
2024-08-30 13:37:46 -0300

    Log Level

---
+++ elixir/log-level/lib/log_level.ex
@@ -0,0 +1,33 @@
+defmodule LogLevel do
+  def to_label(level, legacy?) when legacy? do
+    cond do
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      true -> :unknown
+    end
+  end
+
+  def to_label(level, legacy?) when not legacy? do
+    cond do
+      level == 0 -> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      level == 5 -> :fatal
+      true -> :unknown
+    end
+  end
+
+  def alert_recipient(level, legacy?) do
+    label = to_label(level, legacy?)
+    cond do
+      label in [:error, :fatal] -> :ops
+      label in [:unknown] && legacy? -> :dev1
+      label in [:unknown] -> :dev2
+      true -> false
+    end
+  end
+end
